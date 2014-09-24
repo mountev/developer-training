dac740fdebc4d061a655c9d80492663571e7df2c
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2014-09-24 13:58:53 +0100

    towards apis

--- extensions/uk.co.vedaconsulting.training/CRM/Training/BAO/Settings.php
+++ extensions/uk.co.vedaconsulting.training/CRM/Training/BAO/Settings.php
@@ -10,4 +10,16 @@ class CRM_Training_BAO_Settings extends CRM_Training_DAO_Settings {
     $instance->copyValues($params);
     $instance->save();
   }
+
+  public static function getValues($params) {
+    $values   = array();
+
+    $instance = new crm_training_dao_settings();
+    $instance->copyvalues($params);
+
+    if ($instance->find()) {
+      CRM_Core_DAO::storeValues($instance, $values);
+    }
+    return $values;
+  }
 }
---
+++ extensions/uk.co.vedaconsulting.training/api/v3/Training.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Mailchimp API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRM/API+Architecture+Standards
+ */
+ 
+/**
+ * Training Settings create API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */ 
+function civicrm_api3_training_create($params) {
+  $result = CRM_Training_BAO_Settings::create($params);
+
+  if ($result['is_error'] == 0) {
+    return civicrm_api3_create_success();
+  }
+  else {
+    return civicrm_api3_create_error();
+  }
+}
+
+function civicrm_api3_training_get($params) {
+  $result = CRM_Training_BAO_Settings::getValues($params);
+
+  if ($result['is_error'] == 0) {
+    return civicrm_api3_create_success();
+  }
+  else {
+    return civicrm_api3_create_error();
+  }
+}
